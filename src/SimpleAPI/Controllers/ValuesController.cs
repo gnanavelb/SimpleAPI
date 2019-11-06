2fc98137bc86532992b6c5df4a9d61f6f61fc4da
Balasubramaniyam Gnanavel <bgn2@np.edu.sg>
2019-11-06 11:13:02 +0800

    Borken unit test :(

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Gnanavel";
+            return "Sudha";
         }
 
         // POST api/values
